0318af42e9650a58fc8a2904eba4ab98f9c4a15f
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-14 00:05:15 -1000

    Added rectangle skeleton

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,24 +1,19 @@
 function PixelPainter(width,height){
+//////////////Nodes that interact with DOM///////////////////
 var ppHtmlDisplayEl = document.getElementById('pixelPainter');
 var ppDisplayEl = document.createElement('div');
-ppDisplayEl.innerHTML = 'test';
+var ppCanvasEl = document.createElement('canvas');
+var ppContext = ppCanvasEl.getContext('2d');
+/////////////////////////////////////////////////////////////
 
 
+ppContext.fillStyle = "#FF0000";
+ppContext.rect(0,0,32,32);
+ppContext.fill();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+ppDisplayEl.appendChild(ppCanvasEl);
 ppHtmlDisplayEl.appendChild(ppDisplayEl);
 }
 
+PixelPainter(2,4);
