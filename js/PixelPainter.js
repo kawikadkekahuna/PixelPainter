a807f694721fb2ae4d40ce419f8e705bb7491288
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-15 12:29:49 -1000

    added boxes

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,18 +1,27 @@
 function PixelPainter(width,height){
 //////////////Nodes that interact with DOM///////////////////
+var TOTAL_PIXEL = width * height;
 var ppHtmlDisplayEl = document.getElementById('pixelPainter');
 var ppDisplayEl = document.createElement('div');
+var ppFragmentEl = document.createElement('fragment');
 var ppCanvasEl = document.createElement('canvas');
 var ppContext = ppCanvasEl.getContext('2d');
 /////////////////////////////////////////////////////////////
 
+// ppContext.fillStyle = "#FF0000";
+// ppContext.rect(0,0,32,32);
+// ppContext.fill();
 
-ppContext.fillStyle = "#FF0000";
-ppContext.rect(0,0,32,32);
-ppContext.fill();
+
+for(var i = 0; i< TOTAL_PIXEL; i++){
+  var tmpDiv = document.createElement('div');
+  tmpDiv.className = 'ppBox';
+  ppFragmentEl.appendChild(tmpDiv);
+
+}
 
 
-ppDisplayEl.appendChild(ppCanvasEl);
+ppDisplayEl.appendChild(ppFragmentEl);
 ppHtmlDisplayEl.appendChild(ppDisplayEl);
 }
 
