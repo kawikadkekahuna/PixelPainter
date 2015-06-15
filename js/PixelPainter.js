1e2c8659ae3582a51cbc57f6f1885179e5895a09
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-15 13:15:53 -1000

    added main grid

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,27 +2,36 @@ function PixelPainter(width,height){
 //////////////Nodes that interact with DOM///////////////////
 var TOTAL_PIXEL = width * height;
 var ppHtmlDisplayEl = document.getElementById('pixelPainter');
-var ppDisplayEl = document.createElement('div');
+var ppMainDisplayEl = document.createElement('div');
 var ppFragmentEl = document.createElement('fragment');
-var ppCanvasEl = document.createElement('canvas');
-var ppContext = ppCanvasEl.getContext('2d');
 /////////////////////////////////////////////////////////////
 
-// ppContext.fillStyle = "#FF0000";
-// ppContext.rect(0,0,32,32);
-// ppContext.fill();
-
-
-for(var i = 0; i< TOTAL_PIXEL; i++){
-  var tmpDiv = document.createElement('div');
-  tmpDiv.className = 'ppBox';
-  ppFragmentEl.appendChild(tmpDiv);
-
+for(var i = 0; i< height; i++){
+  var ppTmpRow = document.createElement('div');
+  for(var j = 0; j < width; j++){
+    var block = document.createElement('div');
+    block.className = 'ppBox';
+    ppTmpRow.appendChild(block);
+  }
+  ppMainDisplayEl.appendChild(ppTmpRow);
 }
 
 
-ppDisplayEl.appendChild(ppFragmentEl);
-ppHtmlDisplayEl.appendChild(ppDisplayEl);
+
+
+
+
+
+ppMainDisplayEl.appendChild(ppFragmentEl);
+ppHtmlDisplayEl.appendChild(ppMainDisplayEl);
 }
 
-PixelPainter(2,4);
+PixelPainter(10,10);
+
+
+
+
+
+//   var tmpDiv = document.createElement('div');
+//   tmpDiv.className = 'ppBox';
+//   ppFragmentEl.appendChild(tmpDiv);
