422a58eb9302b1a9f0e06f9814b0bcbd63ad8bae
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-15 15:39:08 -1000

    added validation for object input

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,93 +1,32 @@
 function PixelPainter(width,height){
 //////////////Main DOM Nodes & Variables///////////////////
-var TOTAL_PIXEL = width * height;
-var counter = 0;
-var ppTmpRow,singleBoxEl,i,j;
-var ppHtmlDisplayEl = document.getElementById('pixelPainter');
-var ppMainDisplayEl = document.createElement('div');
-var ppColorDisplayEl = document.createElement('div'); 
-var ppColorArray = ['black','blue','red','orange','green','yellow','aqua','white',];
-var ppColorRunner = 0;
-var currentColorSelected;
+var WIDTH;
+var HEIGHT;
+var GRIDSIZE; //Default 25
+var colorSwatch;
 /////////////////////////////////////////////////////////////
-
-//Generates main grid and appends to MainDisplayEl
-for(i = 0; i< height; i++){
-  ppTmpRow = document.createElement('div');
-  
-  for(j = 0; j < width; j++){
-    //Attributes
-    singleBoxEl = document.createElement('div');
-    singleBoxEl.className = 'ppMainBox';
-    singleBoxEl.id = 'box' + counter;
-
-    //Sets the bg to current color selected;
-    singleBoxEl.addEventListener('click',function(){
-      this.style.background = currentColorSelected;
-    })
-
-    counter++;
-    ppTmpRow.appendChild(singleBoxEl);
-  }
-  ppMainDisplayEl.appendChild(ppTmpRow);
-}
-
-counter = 0; // RESETS COUNTER;
-
-//Generates color grid and appends to ColorDisplayEl
-for(i = 0 ; i <4 ; i++){
-  ppTmpRow = document.createElement('div');
-  for(j = 0; j < 2; j++){
-    singleBoxEl = document.createElement('div');
-
-    //Attributes
-    singleBoxEl.color = ppColorArray[ppColorRunner];
-    singleBoxEl.className = 'ppColorBox'
-    singleBoxEl.id = 'color'+counter;
-    singleBoxEl.style.background = ppColorArray[ppColorRunner];
-
-    //On Click
-    singleBoxEl.addEventListener('click',function(){
-      currentColorSelected = this.color;
-    })
-
-    //Counters
-    ppColorRunner++;
-    counter++;
-
-    ppTmpRow.appendChild(singleBoxEl);
-  }
-  ppColorDisplayEl.appendChild(ppTmpRow);
-}
-
-//Functionality Buttons
-var ppClearBtn = document.createElement('Button');
-    ppClearBtn.innerHTML = 'CLEAR';
-    ppClearBtn.addEventListener('click',function(){
-    for(i = 0; i< TOTAL_PIXEL; i++){
-      document.getElementById('box'+i).style.background='transparent';
+switch(arguments.length){
+    case 1: 
+    if(typeof(width) === 'object'){
+      if(width.hasOwnProperty('height') && width.hasOwnProperty('width') && width.hasOwnProperty('gridSize') && width.hasOwnProperty('colorSwatch')){
+      this.WIDTH = width.width;
+      this.HEIGHT = width.height;
+      this.GRIDSIZE = width.gridSize;
+      this.colorSwatch = width.colorSwatch;
+      break;
+      }
+    }else{
+      throw new TypeError('Invalid arguments');
+      break;
     }
-  });
 
-var ppEraseBtn = document.createElement('Button');
-    ppEraseBtn.innerHTML = 'ERASE';
-    ppEraseBtn.addEventListener('click',function(){
-    currentColorSelected = 'transparent';
-  });
+    case 2:
+    if(typeof(width) === 'number' && typeof(height) === 'number'){
+      break;
+    }else{ 
+      throw new TypeError('Invalid arguments');
+    }
 
-
-ppHtmlDisplayEl.appendChild(ppClearBtn);
-ppHtmlDisplayEl.appendChild(ppEraseBtn);
-ppHtmlDisplayEl.appendChild(ppColorDisplayEl);
-ppHtmlDisplayEl.appendChild(ppMainDisplayEl);
+  }  
 }
 
-PixelPainter(10,10);
-
-
-
-
-
-//   var tmpDiv = document.createElement('div');
-//   tmpDiv.className = 'ppBox';
-//   ppFragmentEl.appendChild(tmpDiv);
