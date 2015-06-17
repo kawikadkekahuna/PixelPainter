156e2d46b3382e6450a1fcebf553ce794ee496b9
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-17 11:15:25 -1000

    added assets / layout folder.  cleaned up code

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -11,7 +11,7 @@ var ppColorDisplayEl = document.createElement('div');
 var WIDTH;
 var HEIGHT;
 var GRIDSIZE = 25; //Default 25
-var COLORSWATCH = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];;
+var COLORSWATCH;
 /////////////////////////////////////////////////////////////
 
 switch(arguments.length){
@@ -67,9 +67,9 @@ for(i = 0; i< this.HEIGHT; i++){
 
 
 //Generates Color Swatch
-for(i = 0 ; i <this.COLORSWATCH.length / 4 ; i++){
+for(i = 0 ; i <this.COLORSWATCH.length / 6 ; i++){
   ppTmpRow = document.createElement('div');
-  for(j = 0; j < 4; j++){
+  for(j = 0; j < 6; j++){
     singleBoxEl = document.createElement('div');
 
     //Attributes
@@ -127,8 +127,8 @@ ppHtmlDisplayEl.appendChild(ppMainDisplayEl);
 
 //////Testing//////////
 var testObj = {
-  width: 25,
-  height: 25,
+  width: 10,
+  height: 10,
   gridSize: 10,
   colorSwatch: ['red','blue','green','purple','orange','yellow',"AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
 }
